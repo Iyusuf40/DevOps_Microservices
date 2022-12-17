1204ddbdd4d434eed0c0ab567b77d41a0b4d64ee
Iyusuf40 <punkgts40@gmail.com>
2022-12-17 20:10:28 +0000

    push image to docker hub

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=iyusuf40/machine-learning-app
 
 # Step 2:  
 # Authenticate & tag
+docker tag proj $dockerpath
+docker login
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
